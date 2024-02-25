d3d3f696851f8789904b9d3d14f3dd08ec5b1220
akash <akash_196003@saitm.org>
2024-02-25 22:33:11 +0530

    validaotr chanfef

--- invoice/models.py
+++ invoice/models.py
@@ -17,8 +17,8 @@ class InvoiceDetail(models.Model):
     )
     description = models.CharField(max_length=255)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
 
     def __str__(self):
         return f"Detail for Invoice #{self.invoice.pk}: {self.description}"
